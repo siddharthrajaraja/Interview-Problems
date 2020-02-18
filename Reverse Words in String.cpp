969b602e2b86adb3df87a4a88156d27663bd32f9
SIDDHARTH RAMESH CHAND RAJA <45534008+siddharthrajaraja@users.noreply.github.com>
2020-02-19 03:03:04 +0530

    updated some changes

--- Reverse Words in String.cpp
+++ Reverse Words in String.cpp
@@ -8,6 +8,12 @@ int  main(){
 	int i=strlen(s)-1;
 	int j;
 	int end;
+    int index=0;
+    char ans[strlen(s)];
+
+    while(s[i]==' ' && i>=0)i--;
+
+
 	while (i>=0){
 		j=i;
 		int k;
@@ -16,15 +22,19 @@ int  main(){
 			if(s[j]==' ')break;
 			j--;
 		}
-		if (i!=strlen(s)-1)printf(" ");
+		if (i!=strlen(s)-1)ans[index++]=' ';
 		//printf("%d",j);
 		for(k=j+1;k<=i;k++){
-			printf("%c",s[k]);
+			//printf("%c",s[k]);
+            ans[index++]=s[k];
+
 		}
 		//printf("\n%d\n",k);
 		i=j-1;
 		//printf("%d",i);
 	}
+    puts(ans);
+    
 	return 0;
 }
 
